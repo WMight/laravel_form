d7f98f725dcdbeb01fcd8075ca3e511d514eda3b
WMight <wwwyf123456789@163.com>
2020-07-28 10:17:44 +0800

    表单验证

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Student;
 use Illuminate\Http\Request;
+
 class StudentController extends Controller
 {
     //学生列表页
@@ -17,6 +18,39 @@ class StudentController extends Controller
     public function create(Request $request)
     {
         if($request->isMethod('POST')){
+            //控制器验证
+            /*$this->validate($request,[
+                "Student.name" =>'required|min:2|max:20',
+                "Student.age" =>'required|integer',
+                "Student.sex" =>'required|integer',
+            ],[
+                'required'=>':attribute 为必填项',
+                'min'=>':attribute 长度不符合要求',
+                'integer'=>':attribute 必须为整数',
+            ],[
+                'Student.name'=>'姓名',
+                'Student.age'=>'年龄',
+                'Student.sex'=>'性别',
+            ]);*/
+
+            //Validator类验证
+            $validator = \Validator::make($request->input(),[
+                "Student.name" =>'required|min:2|max:20',
+                "Student.age" =>'required|integer',
+                "Student.sex" =>'required|integer',
+            ],[
+                'required'=>':attribute 为必填项',
+                'min'=>':attribute 长度不符合要求',
+                'integer'=>':attribute 必须为整数',
+            ],[
+                'Student.name'=>'姓名',
+                'Student.age'=>'年龄',
+                'Student.sex'=>'性别',
+            ]);
+            if($validator->fails()){
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
             $data = $request->input('Student');
             if(Student::create($data)){
                 return redirect('student/index')->with('success','添加成功！');
--- resources/views/student/create.blade.php
+++ resources/views/student/create.blade.php
@@ -1,7 +1,7 @@
 @extends('common.layouts')
 
 @section('content')
-    
+    @include('common.validator')
 <!-- 自定义内容区域 -->
  <div class="panel panel-default">
         <div class="panel-heading">新增学生</div>
@@ -10,14 +10,14 @@
             <!-- 生成隐藏的表单带有Token字段 -->
                 {{csrf_field()}}
                 <div class="form-group"><label for="name" class="col-sm-2 control-label">姓名</label>
-                    <div class="col-sm-5"><input type="text" name="Student[name]" class="form-control" id="name"
+                    <div class="col-sm-5"><input type="text" value="{{old('Student')['name']}}" name="Student[name]" class="form-control" id="name"
  placeholder="请输入学生姓名"></div>
-                    <div class="col-sm-5"><p class="form-control-static text-danger">姓名不能为空</p></div>
+                    <div class="col-sm-5"><p class="form-control-static text-danger">{{$errors->first('Student.name')}}</p></div>
                         </div>
                 <div class="form-group"><label for="age" class="col-sm-2 control-label">年龄</label>
-                    <div class="col-sm-5"><input type="text" name="Student[age]" class="form-control" id="age"
+                    <div class="col-sm-5"><input type="text"  value="{{old('Student')['age']}}" name="Student[age]" class="form-control" id="age"
  placeholder="请输入学生年龄"></div>
-                    <div class="col-sm-5"><p class="form-control-static text-danger">年龄只能为整数</p></div>
+                    <div class="col-sm-5"><p class="form-control-static text-danger">{{$errors->first('Student.age')}}</p></div>
                         </div>
                 <div class="form-group"><label for="sex" class="col-sm-2 control-label">性别</label>
                     <div class="col-sm-5"><label class="radio-inline"> <input type="radio" name="Student[sex]"
@@ -25,7 +25,7 @@
                 <label class="radio-inline"> <input type="radio" name="Student[sex]" value="20"> 男 </label>
                 <label class="radio-inline"> <input type="radio" name="Student[sex]" value="30"> 女 </label>
                             </div>
-            <div class="col-sm-5"><p class="form-control-static text-danger">请选择性别</p></div>
+            <div class="col-sm-5"><p class="form-control-static text-danger">{{$errors->first('Student.sex')}}</p></div>
         </div>
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
